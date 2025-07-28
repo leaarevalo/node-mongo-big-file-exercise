ea220461978c19c1c0bc4a0cfea2aef5d3736996
LeaArevaloSky <user_acid50@skyairline.com>
2025-07-27 21:14:29 -0300

    :sparkles: file read test done

--- src/controller.js
+++ src/controller.js
@@ -1,27 +1,70 @@
-const Records = require('./records.model');
+const Records = require("./records.model");
+const { parse } = require("fast-csv");
+const fs = require("fs");
 
+const batchSize = 1000; 
 const upload = async (req, res) => {
-    const {file} = req;
+  const { file } = req;
+  let data = [];
+  let processing = Promise.resolve();
+  let insertedCount = 0;
+  const stream = fs.createReadStream(file.path);
 
-    /* Acá va tu código! Recordá que podés acceder al archivo desde la constante file */
+  stream
+    .pipe(parse({ headers: true, skipEmptyLines: true }))
+    .on("data", (row) => {
+      data.push(row);
+      if (data.length >= batchSize) {
+        const batch = [...data];
+        data = [];
+        stream.pause();
 
-    return res.status(200).json({ message: 'some response' });
+        processing = processing
+          .then(() => {
+            return Records.insertMany(batch, { ordered: false });
+          })
+          .then((result) => {
+            insertedCount += result.length;
+            stream.resume();
+          })
+          .catch((err) => {
+            stream.destroy();
+            throw err;
+          });
+      }
+    })
+    .on("end", async () => {
+      await processing;
+      if (data.length > 0) {
+            const result =  await Records.insertMany(data, { ordered: false });
+            insertedCount += result.length;
+      }
+      fs.unlink(file.path, (err) => {
+        if (err) {
+          console.error("Error deleting file:", err);
+        }
+      });
+      return res.status(200).json({
+        message: "File processed successfully",
+        inserted: insertedCount,
+      });
+    })
+    .on("error", (err) => {
+      return res.status(500).json({ error: "Error processing file" });
+    });
 };
 
 const list = async (_, res) => {
-    try {
-        const data = await Records
-            .find({})
-            .limit(10)
-            .lean();
-        
-        return res.status(200).json(data);
-    } catch (err) {
-        return res.status(500).json(err);
-    }
+  try {
+    const data = await Records.find({}).limit(10).lean();
+
+    return res.status(200).json(data);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
 module.exports = {
-    upload,
-    list,
+  upload,
+  list,
 };
